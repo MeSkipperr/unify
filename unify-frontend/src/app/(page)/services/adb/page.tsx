c55059ef83241900df640c7779d6da4509b64eaf
MeSkipperr <ikadekyolaandika02@gmail.com>
2026-02-21 17:19:36 +0800

    fix: remove Suspense wrapper from AdbTable component

--- unify-frontend/src/app/(page)/services/adb/page.tsx
+++ unify-frontend/src/app/(page)/services/adb/page.tsx
@@ -1,16 +1,13 @@
 "use client"
 
 import AdbTable from "@/features/adb/components/table"
-import { Suspense } from "react";
 
 const AdbPage = () => {
 
 
     return (
         <div className="h-[85dvh]">
-            <Suspense fallback={<div>Loading...</div>}>
-                <AdbTable serviceType="manual" addNewData />
-            </Suspense>
+            <AdbTable serviceType="manual" addNewData />
         </div>
 
     );
